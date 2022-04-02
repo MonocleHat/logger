874338ec0f3c8dd92bf3b91b5c6c2a7c14b22f7c
Brandyn P <brandyn.pereira@gmail.com>
2022-04-01 23:01:49 -0400

    0.0.2 -- Starting to implement Logger.h/Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -1,1 +1,70 @@
-/*Start of code*/
+#include <iostream>
+#include <stdlib.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include "Logger.h"
+using namespace std;
+#define PORT 9999
+#define BUFLEN 512
+#define IP "127.0.0.1" // change me
+//Globals
+LOG_LEVEL severity;
+int sockfd, len; 
+struct sockaddr_in listener;
+pthread_mutex_t logLock;
+pthread_t logthread;
+bool is_running;
+
+void *recv_func(void *arg);
+int InitializeLog(){
+    //create socket
+    memset(&listener, 0,sizeof(listener));
+    listener.sin_addr.s_addr = inet_addr(IP);
+    listener.sin_port = htons(PORT);
+    listener.sin_family = AF_INET;
+    sockfd = socket(AF_INET,SOCK_DGRAM,0);
+
+    bind(sockfd,(struct sockaddr*)&listener,sizeof(listener));
+    pthread_mutex_init(&logLock, NULL);
+    //set address and port of server
+    //create pthread_mutex_t
+    //start rec thread pass file descriptor to it
+    is_running = true;
+    pthread_create(&logthread, NULL, recv_func, &sockfd);
+}
+
+void SetLogLevel(LOG_LEVEL level){
+    severity = level; 
+}
+
+void Log(LOG_LEVEL level, const char *prog, const char *func, int line, const char *message){
+    char BUF[BUFLEN];
+    int len;
+    time_t now = time(0);
+    char *dt = ctime(&now);
+    memset(BUF,0,BUFLEN);
+    char levelStr[][16]={"DEBUG","WARNING","ERROR","CRITICAL"};
+    len = sprintf(BUF, "%s %s %s:%s:%d %s\n",dt,levelStr[level],prog,func,line,message)+1;
+    BUF[BUFLEN-1]='\0';
+    sendto(sockfd,BUF,len,0,(struct sockaddr*)&listener,sizeof(listener));
+}
+
+void ExitLog(){
+    is_running = false;
+}
+
+void *recv_func(void *arg){
+    char BUF[BUFLEN];
+    int ret, len;
+    int servSock = *(int *)arg;
+    while(is_running){
+        
+    }
+
+}
--- Logger.h
+++ Logger.h
@@ -1,1 +1,29 @@
 /*Start of code*/
+#include <iostream>
+
+enum LOG_LEVEL{
+    DEBUG,
+    WARNING,
+    ERROR,
+    CRITICAL
+};
+
+//functions
+
+/* Create a non blocking socket for UDP
+* set address and port of server
+* create mutex to protect any hared resources
+* start recieve thread, pass file descriptor to it*/
+int InitializeLog();
+
+//Set filter log level, store in a variable global within Logger.cpp
+void SetLogLevel(LOG_LEVEL level);
+
+/*Compare the severity of log to the filter log severity - log is discarded if severity is lower
+* create timestamp to be added to log message
+* apply mutexing to shared resources
+* message sent to server via UDP "sendto"*/
+void Log(LOG_LEVEL level, const char *prog, const char* func,int line, const char *message);
+
+//change an is_running flag, closing the descriptor 
+void ExitLog();
